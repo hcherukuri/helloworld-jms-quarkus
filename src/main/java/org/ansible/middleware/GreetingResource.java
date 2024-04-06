02cd6178465b69e9aa948ab7a8d3b47c00081c4b
Harsha Cherukuri <hcheruku@redhat.com>
2024-04-06 17:38:56 -0400

    Update provide URL

--- src/main/java/org/ansible/middleware/GreetingResource.java
+++ src/main/java/org/ansible/middleware/GreetingResource.java
@@ -28,8 +28,8 @@ public class GreetingResource {
     private static final String DEFAULT_USERNAME = "quickstartUser";
     private static final String DEFAULT_PASSWORD = "quickstartPwd1!";
     private static final String INITIAL_CONTEXT_FACTORY = "org.wildfly.naming.client.WildFlyInitialContextFactory";
-    private static final String PROVIDER_URL = "http-remoting://127.0.0.1:8080";
-
+    // private static final String PROVIDER_URL = "http-remoting://127.0.0.1:8080";
+    private static final String PROVIDER_URL = "http-remoting://"+ System.getenv("wildfly_vm_ip")+ ":8080";
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
